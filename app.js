e0d36ce8b2aa6eb8b4321604f93914d5b61d685f
Chris Lane <pankokrumbs@gmail.com>
2020-01-07 15:11:37 -0700

    Create development port for server

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-function sayHello(name) {
-  console.log('Hello ' + name)
-}
+const express = require('express');
+const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+// const cors = require(cors);
 
-// sayHello('Chris');
+app.set('port', 3001);
+app.use(express.json());
+// app.use(cors());
+
+app.get('/', (request, response) => {
+  response.send('Hey maybe you get job already')
+});
+
+module.exports = app;
